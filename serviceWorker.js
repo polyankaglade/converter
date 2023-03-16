f428b57e992e4bf6fbfdba175359e4c6016c9d17
polyankaglade <off.polyanskaya.a@gmail.com>
2023-03-16 10:28:15 +0100

    upload

---
+++ serviceWorker.js
@@ -0,0 +1,23 @@
+const staticDevConverter = "dev-converter-site-v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevConverter).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
